65bf4fd8c97a42af8a5ce1b079764b7b4c0a3cd1
Gabriel Schiavo <gabriel@lewagon.org>
2018-10-29 12:15:05 -0200

    Add query string

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'ask', to: 'questions#ask'
-  get 'answear', to: 'questions#answear'
+  get 'ask', to: 'questions#ask', as: 'ask'
+  get 'answear', to: 'questions#answear', as: 'answear'
+
+  root to: "questions#ask"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
