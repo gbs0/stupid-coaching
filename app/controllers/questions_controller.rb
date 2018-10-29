0b9d4f33fe8d5d4094ac934b0bf3be8bcbeed0c7
Gabriel Schiavo <gabriel@lewagon.org>
2018-10-29 13:01:38 -0200

    Add answer function in question route

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,30 @@
 class QuestionsController < ApplicationController
   def ask
-    @question = params[:question]
   end
 
   def answer
+    @user_input = params[:question]
+    @question = coach_answer(params[:question])
   end
+
+  def coach_answer(question)
+    if question.include?('?')
+      return "Silly question, get dressed and go to work!"
+    elsif question == "I am going to work right now!"
+      "Great!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+
+  # def coach_answer_enhanced(question)
+  #   if question == "I AM GOING TO WORK RIGHT NOW!"
+  #     ""
+  #   elsif question.upcase
+  #     return "I can feel your motivation! #{coach_answer(question)}"
+  #   else
+  #     return coach_answer(question)
+  #   end
+  # end
+
 end
